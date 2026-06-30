8352e3be760ec4e8ef82e238a161241cad6d8860
Erwin Wilson Ceniza <erwinwilsonceniza@gmail.com>
2026-06-30 20:20:12 +0800

    chore: remove deprecated memory setting, suppress chunk size warning

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 600,
+  },
   server: {
     headers: {
       'Permissions-Policy': 'camera=(), microphone=(), geolocation=()'
